29d03c322791a8ebe0c52a30fb67f56102b9b2ce
potados99 <potados99@gmail.com>
2020-04-04 20:23:17 +0900

    Fix.

--- config.mjs
+++ config.mjs
@@ -1,9 +1,24 @@
+if (process.env.NODE_ENV === 'production') {
+    if (!process.env.REDIS_URL) {
+        throw new Error('No REDIS_URL');
+    }
+    if (!process.env.SLACK_TOKEN) {
+        throw new Error('No SLACK_TOKEN');
+    }
+    if (!process.env.NOTICE_CHANNEL) {
+        throw new Error('No NOTICE_CHANNEL');
+    }
+    if (!process.env.SLACK_SIGNING_SECRET) {
+        throw new Error('No SLACK_SIGNING_SECRET');
+    }
+}
+
 export default {
     port: process.env.PORT || 3000,
-    redis_url: process.env.REDIS_URL || throw new Error('No REDIS_URL'),
-    slack_token: process.env.SLACK_TOKEN || throw new Error('No SLACK_TOKEN'),
-    notice_channel: process.env.NOTICE_CHANNEL || throw new Error('No NOTICE_CHANNEL'),
-    slack_signing_secret: process.env.SLACK_SIGNING_SECRET || throw new Error('No SLACK_SIGNING_SECRET'),
+    redis_url: process.env.REDIS_URL || 'url',
+    slack_token: process.env.SLACK_TOKEN || 'token',
+    notice_channel: process.env.NOTICE_CHANNEL || 'channel',
+    slack_signing_secret: process.env.SLACK_SIGNING_SECRET || 'secret',
 
     report_must_include: [
         '[진행상황]',
