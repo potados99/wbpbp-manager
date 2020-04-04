5c7e087300e74d18fca3d9b5fd3b6a05015cbed6
potados99 <potados99@gmail.com>
2020-04-04 20:15:55 +0900

    Done config.

--- config.mjs
+++ config.mjs
@@ -1,17 +1,10 @@
-if (!process.env.REDIS_URL) {
-    throw new Error('No REDIS_URL');
-}
-if (!process.env.SLACK_TOKEN) {
-    throw new Error('No SLACK_TOKEN');
-}
-if (!process.env.NOTICE_CHANNEL) {
-    throw new Error('No NOTICE_CHANNEL');
-}
-if (!process.env.SLACK_SIGNING_SECRET) {
-    throw new Error('No SLACK_SIGNING_SECRET');
-}
-
 export default {
+    port: process.env.PORT || 3000,
+    redis_url: process.env.REDIS_URL || throw new Error('No REDIS_URL'),
+    slack_token: process.env.SLACK_TOKEN || throw new Error('No SLACK_TOKEN'),
+    notice_channel: process.env.NOTICE_CHANNEL || throw new Error('No NOTICE_CHANNEL'),
+    slack_signing_secret: process.env.SLACK_SIGNING_SECRET || throw new Error('No SLACK_SIGNING_SECRET'),
+
     report_must_include: [
         '[진행상황]',
         '[진행 상황]',
@@ -20,4 +13,3 @@ export default {
     urge_start_at: 15, /* 3pm */
     report_minimum_length: 10,
 };
-
